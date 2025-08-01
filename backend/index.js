76a501068443a2a557058d736c911c3a8bd9d1f7
Maitre <maitre@Air-de-Maitre.lan>
2025-08-01 14:14:26 +0200

    Correction du mot de passe d'application Gmail

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'sebastien95360@gmail.com',
-    pass: 'knrw bqjk jfmq gezy'
+    pass: 'knrwbqjkjfmqgezy'
   }
 });
 
